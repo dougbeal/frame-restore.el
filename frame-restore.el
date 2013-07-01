68fb11eb1d516c5b3e3f957f360b1ad9c8dfabd4
Sebastian Wiesner <lunaryorn@gmail.com>
2013-07-01 15:10:19 +0200

    Bump master version

--- frame-restore.el
+++ frame-restore.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
 ;; URL: https://github.com/lunaryorn/frame-restore.el
 ;; Keywords:  frames convenience
-;; Version: 0.1
+;; Version: 0.2-cvs
 ;; Package-Requires: ((dash "1.2") (emacs "24.1"))
 
 ;; This file is not part of GNU Emacs.
